7b2e9bde6f7b3ba5b2c9040eaedfbe7adfaf81cc
Antoine Castel <antoine.castel@epsi.fr>
2023-03-08 16:20:47 +0100

    add Bryan UI + fct DbOpenHelper

---
+++ src/main/java/com/example/test_mspr_produit/db/DbOpenHelper.java
@@ -0,0 +1,83 @@
+package com.example.test_mspr_produit.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DbOpenHelper {
+    public static DbOpenHelper instance = null;
+    private Connection cnx = null;
+    private DbOpenHelper() {
+        String cnxStr = "jdbc:mariadb://127.0.0.1:3306/mspr";
+        try {
+            this.cnx = DriverManager.getConnection(cnxStr,"root", "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getCnx() {
+        return this.cnx;
+    }
+
+    public static DbOpenHelper getInstance(){
+        if (DbOpenHelper.instance == null) {
+            synchronized (DbOpenHelper.class) {
+                if (DbOpenHelper.instance == null){
+                    DbOpenHelper.instance = new DbOpenHelper();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void add_user(String firstname, String lastname, String login, String password) {
+        String sql_req = "INSERT INTO users(login, password) VALUES (?, ?)";
+        try {
+            PreparedStatement statement = cnx.prepareStatement(sql_req);
+            statement.setString(1, login);
+            statement.setString(2, password);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update_user(int id, String firstname, String lastname, String login, String password) {
+        String sql_req = "UPDATE `users` SET `firstname` = ?, `lastname` = ?, `login` = ?, `password` = ? WHERE `users`.`id_users` = ?";
+        try {
+            PreparedStatement statement = cnx.prepareStatement(sql_req);
+            statement.setString(1, firstname);
+            statement.setString(2, lastname);
+            statement.setString(3, login);
+            statement.setString(4, password);
+            statement.setInt(5, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void show_client(int id) {
+        String sql_req = "SELECT * FROM xx WHERE id = ?";
+        try {
+            PreparedStatement statement = cnx.prepareStatement(sql_req);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void del_xx(int id) {
+        String sql_req = "DELETE FROM xx WHERE id = ?";
+        try {
+            PreparedStatement statement = cnx.prepareStatement(sql_req);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
